339958a08f0492e3c0aaf2fcbd7f2bd10f1d24d1
Jufm7 <julianafm276@gmail.com>
2025-05-30 17:33:10 -0400

    Primeiro commit

---
+++ conceitos/async-await.js
@@ -0,0 +1,7 @@
+// async - assíncrono
+async function exibirNomeDogFormatado (nome) {
+    return nome.toUpperCase();
+  }
+ 
+// await - espera o resultado de uma promise
+console.log(await exibirNomeDogFormatado(`Pipoca`))
